04939cc0c8c72806ac5ddc468c46a6c4da2e55bc
Florian Gilcher <florian.gilcher@asquera.de>
2017-05-29 10:33:36 +0200

    Follow framework changes

--- app/app.rs
+++ app/app.rs
@@ -3,11 +3,18 @@ extern crate gerust_controller;
 extern crate gerust_router;
 extern crate gerust_context;
 
-use std::sync::Arc;
 use gerust_controller::{Controller, ControllerFactory};
 use gerust_router::Params;
 use gerust_context::Context;
 
-struct App<C: Context> {
-    controllers: Vec<Arc<ControllerFactory<C, Controller=Controller<C, Params=Params, Result=Params>>>>
+pub struct App<C: Context> {
+    controllers: Vec<Box<ControllerFactory<C, Controller=Controller<C, Params=Params, Result=Params>>>>,
+}
+
+impl<C: Context> gerust_app::App for App<C> {
+    type ControllerFactory = ControllerFactory<C, Controller=Controller<C, Params=Params, Result=Params>>;
+
+    fn controllers(&self) -> &[Box<Self::ControllerFactory>] {
+        &self.controllers
+    }
 }
