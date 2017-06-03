210b875e0c562b45f3ddded8a6657d895441c3fd
Florian Gilcher <florian.gilcher@asquera.de>
2017-06-03 17:18:40 +0300

    Well, this works

--- app/app.rs
+++ app/app.rs
@@ -8,7 +8,7 @@ use gerust_router::Params;
 use gerust_context::Context;
 
 pub struct App<'factories, 'controllers, C: Context> {
-    controllers: Vec<Box<ControllerFactory<'factories, 'controllers, C, Params, Params, Box<Controller<'controllers, C, Params=Params, Result=Params> + 'controllers>> + 'factories>>,
+    controllers: Vec<Box<ControllerFactory<'factories, 'controllers, C, Params, Params>>>,
 }
 
 impl<'factories, 'controllers, C: Context + 'controllers> App<'factories, 'controllers, C> {
@@ -16,16 +16,11 @@ impl<'factories, 'controllers, C: Context + 'controllers> App<'factories, 'contr
         App { controllers: Vec::new() }
     }
 
-    pub fn add_controller(&mut self, controller: Box<ControllerFactory<'factories, 'controllers, C, Params, Params, Box<Controller<'controllers, C, Params=Params, Result=Params> + 'controllers>> + 'factories>) {
+    pub fn add_controller(&mut self, controller: Box<ControllerFactory<'factories, 'controllers, C, Params, Params>>) {
         self.controllers.push(controller)
     }
 
-    pub fn controllers(&self) -> &[Box<ControllerFactory<'factories, 'controllers, C, Params, Params, Box<Controller<'controllers, C, Params=Params, Result=Params> + 'controllers>> + 'factories>] {
+    pub fn controllers(&self) -> &[Box<ControllerFactory<'factories, 'controllers, C, Params, Params> + 'factories>] {
         self.controllers.as_ref()
     }
-}
-
-
-impl<'factories, 'controllers, C: Context> gerust_app::App for App<'factories, 'controllers, C> {
-
 }
--- config/init.rs
+++ config/init.rs
@@ -3,14 +3,13 @@ use gerust_controller::*;
 use app::App;
 use controllers::echo::EchoController;
 use contexts::HttpContext;
+use gerust_router::Params;
 
 pub fn init() {
     println!("Initializing project");
 
-    let app: App<HttpContext> = App::new();
-    let factory = construct::<HttpContext>()
-                        .for_controller::<EchoController<_>>();
-    
+    let mut app: App<HttpContext> = App::new();
+    let factory = factory::<HttpContext, Params, Params, EchoController<HttpContext>>();
     app.add_controller(Box::new(factory));
 
     routes::draw();
