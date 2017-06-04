23f12f36a07cb6bba6f692b31dec9675c87d8ccb
Florian Gilcher <florian.gilcher@asquera.de>
2017-06-04 17:32:59 +0300

    Introduce ControllerSet

--- app/app.rs
+++ app/app.rs
@@ -3,24 +3,20 @@ extern crate gerust_controller;
 extern crate gerust_router;
 extern crate gerust_context;
 
-use gerust_controller::{Controller, ControllerFactory};
+use gerust_controller::{Controller, ControllerFactory, ControllerSet};
 use gerust_router::Params;
 use gerust_context::Context;
 
 pub struct App<'factories, 'controllers, C: Context> {
-    controllers: Vec<Box<ControllerFactory<'factories, 'controllers, C, Params, Params>>>,
+    controllers: ControllerSet<'factories, 'controllers, C, Params, Params>,
 }
 
 impl<'factories, 'controllers, C: Context + 'controllers> App<'factories, 'controllers, C> {
     pub fn new() -> Self {
-        App { controllers: Vec::new() }
+        App { controllers: ControllerSet::new() }
     }
 
-    pub fn add_controller(&mut self, controller: Box<ControllerFactory<'factories, 'controllers, C, Params, Params>>) {
-        self.controllers.push(controller)
-    }
-
-    pub fn controllers(&self) -> &[Box<ControllerFactory<'factories, 'controllers, C, Params, Params> + 'factories>] {
-        self.controllers.as_ref()
+    pub fn controllers_mut(&mut self) -> &mut ControllerSet<'factories, 'controllers, C, Params, Params> {
+        &mut self.controllers
     }
 }
