0174e93077ceddb0b222f28fd61af9f55cb3401f
Florian Gilcher <florian.gilcher@dm.de>
2017-06-06 09:32:43 +0200

    Move controllers to 'static Context lifetime

--- app/app.rs
+++ app/app.rs
@@ -7,16 +7,16 @@ use gerust_controller::{Controller, ControllerFactory, ControllerSet};
 use gerust_router::Params;
 use gerust_context::Context;
 
-pub struct App<'factories, 'controllers, C: Context> {
-    controllers: ControllerSet<'factories, 'controllers, C, Params, Params>,
+pub struct App<C: Context> {
+    controllers: ControllerSet<C, Params, Params>,
 }
 
-impl<'factories, 'controllers, C: Context + 'controllers> App<'factories, 'controllers, C> {
+impl<C: Context> App<C> {
     pub fn new() -> Self {
         App { controllers: ControllerSet::new() }
     }
 
-    pub fn controllers_mut(&mut self) -> &mut ControllerSet<'factories, 'controllers, C, Params, Params> {
+    pub fn controllers_mut(&mut self) -> &mut ControllerSet<C, Params, Params> {
         &mut self.controllers
     }
 }
--- app/controllers/echo/mod.rs
+++ app/controllers/echo/mod.rs
@@ -3,15 +3,15 @@ use gerust_http::HttpContext;
 // this needs to be replaced
 use gerust_router::Params;
 
-pub struct EchoController<'a, C: HttpContext + 'a> {
-    context: &'a C   
+pub struct EchoController<C: HttpContext> {
+    context: &'static C
 }
 
-impl<'a, C: HttpContext + 'a> Controller<'a, C> for EchoController<'a, C> {
+impl<C: HttpContext> Controller<C> for EchoController<C> {
     type Params = Params;
     type Result = Params;
 
-    fn new(context: &'a C) -> Self { EchoController { context } }
+    fn new(context: &'static C) -> Self { EchoController { context } }
 
     fn execute(&self, input: Params) -> Params {
         let mut params = Params::new();
